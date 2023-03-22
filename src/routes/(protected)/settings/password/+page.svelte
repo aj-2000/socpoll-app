66781b633f7d4221f800fbfe0e0deec1adbdd570
aj2000 <hi@ajaysharma.dev>
2023-03-22 14:41:49 +0530

    WIP: Settings Page

---
+++ src/routes/(protected)/settings/password/+page.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { faker } from '@faker-js/faker'
+</script>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start ">
+		<span class="px-6 py-1 font-bold w-[125px]"
+			><img alt="Ajay Sharma" class="w-10 h-10 rounded-full" src={faker.image.avatar()} /></span
+		>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<span class="text-sm">__aj2000__</span>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-6 py-1 font-bold w-[125px]">Old Password</span>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<input
+				class="input input-sm w-full bg-base-content border-base-300"
+				type="text"
+				id="name"
+				name="name"
+				value="__aj2000__"
+				required
+			/>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-6 py-1 font-bold w-[125px]">New Password</span>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<input
+				class="input input-sm w-full bg-base-content border-base-300"
+				type="text"
+				id="name"
+				name="name"
+				value="__aj2000__"
+				required
+			/>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class=" px-6 py-1 font-bold w-[125px]">Confirm New Password</span>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<input
+				class="input input-sm w-full bg-base-content border-base-300"
+				type="text"
+				id="name"
+				name="name"
+				value="__aj2000__"
+				required
+			/>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-8 py-1 font-bold w-[125px]" />
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<button
+				class="btn btn-sm border-base-300 hover:text-base-content hover:bg-base-300"
+				type="submit">Change Password</button
+			>
+		</div>
+	</div>
+</div>
---
+++ src/routes/(protected)/settings/profile/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { faker } from '@faker-js/faker'
+</script>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start ">
+		<span class="px-6 py-1 font-bold w-[125px]"
+			><img alt="Ajay Sharma" class="w-10 h-10 rounded-full" src={faker.image.avatar()} /></span
+		>
+	</div>
+	<div class="w-full flex flex-col">
+		<div class="w-full">
+			<span class="text-sm">__aj2000__</span>
+		</div>
+		<div class="w-full">
+			<span class="text-sm">Change profile photo</span>
+		</div>
+	</div>
+</div>
+
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-8 py-1 font-bold w-[125px]">Name</span>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<input
+				class="input input-sm w-full bg-base-content border-base-300"
+				type="text"
+				id="name"
+				name="name"
+				value="Ajay Sharma"
+				required
+			/>
+		</div>
+		<div class="text-xs text-base-300">
+			Help people discover your account by using the name you're known by: either your full name,
+			nickname, or business name.
+		</div>
+	</div>
+</div>
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-8 py-1 font-bold w-[125px]">Username</span>
+	</div>
+	<div class="w-full flex flex-col gap-3">
+		<div class="w-full">
+			<input
+				class="input input-sm w-full bg-base-content border-base-300"
+				type="text"
+				id="name"
+				name="name"
+				value="__aj2000__"
+				required
+			/>
+		</div>
+	</div>
+</div>
+<div class="flex w-full">
+	<div class="flex justify-end items-start">
+		<span class="px-8 py-1 font-bold w-[125px]">Bio</span>
+	</div>
+	<div class="w-full flex flex-col gap-2">
+		<div class="w-full">
+			<textarea
+				class="w-full bg-base-content border border-base-300 p-2"
+				id="bio"
+				name="bio"
+				required
+				rows="2"
+				value="Seeker
+Photography account: @_ajay.clicks_"
+			/>
+		</div>
+		<div class="text-xs font-bold text-base-300">42 / 150</div>
+	</div>
+</div>
